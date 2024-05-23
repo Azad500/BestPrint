4c73bdfeb04bd7adbdc96aedccdead93af3a3510
saidisaoglu <seidisaoglu@gmail.com>
2024-05-23 19:02:56 +0400

    said

--- src/Components/AboutUsers/MyAccount.jsx
+++ src/Components/AboutUsers/MyAccount.jsx
@@ -1,6 +1,21 @@
+import { setuserFormData } from "../../ReduxToolkit/Features/StateSlice";
 import styles from "./MyAccount.module.scss";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function MyAccount() {
+  const { userFormData } = useSelector((state) => state.state);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    dispatch(setuserFormData({ [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userFormData);
+  };
+
   return (
     <section className={styles.myAccountPart}>
       <div className={styles.myAccountTexts}>
@@ -13,26 +28,50 @@ export default function MyAccount() {
           </p>
         </div>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <ul className={styles.inputsContainer}>
           <li>
             <div className={styles.inputElement}>
-              <label htmlFor="Adınız">Adınız</label>
-              <input type="text" name="name" id="Adınız" />
+              <label htmlFor="name">Adınız</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={userFormData.name}
+                onChange={handleChange}
+              />
             </div>
             <div className={styles.inputElement}>
-              <label htmlFor="Soyadınız">Soyadınız</label>
-              <input type="text" name="surname" id="Soyadınız" />
+              <label htmlFor="surname">Soyadınız</label>
+              <input
+                type="text"
+                name="surname"
+                id="surname"
+                value={userFormData.surname}
+                onChange={handleChange}
+              />
             </div>
           </li>
           <li>
             <div className={styles.inputElement}>
-              <label htmlFor="Email">Email</label>
-              <input type="email" name="email" id="Email" />
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={userFormData.email}
+                onChange={handleChange}
+              />
             </div>
             <div className={styles.inputElement}>
-              <label htmlFor="Telefon">Əlaqə nömrəsi</label>
-              <input type="number" name="phone" id="Telefon" />
+              <label htmlFor="phone">Əlaqə nömrəsi</label>
+              <input
+                type="number"
+                name="phone"
+                id="phone"
+                value={userFormData.phone}
+                onChange={handleChange}
+              />
             </div>
           </li>
         </ul>
